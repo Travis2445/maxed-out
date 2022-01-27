d9a6cf08e56c78db465cfae5eb8aab68e81468e3
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-01-26 21:03:07 -0600

    updated workout routes

---
+++ controllers/api/workout-routes.js
@@ -0,0 +1,94 @@
+const router = require('express').Router();
+
+const { Workout, Weights, Cardio } = require('../../models');
+
+router.get('/', (req, res) => {
+        // find all wokrouts
+        Workout.findAll(
+        {
+            include: {
+            model: [Cardio, Weights],
+            attributes: ['cardio_type', 'weight_type']
+            }
+        }
+        ).then(workData => res.json(workData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+    });
+
+    router.get('/:id', (req, res) => {
+    // find one workout by its `id` value
+    Workout.findOne(
+        {
+        where: {
+            id: req.params.id
+        },
+        include: {
+            model: [Cardio, Weights],
+            attributes: ['workout_id']
+        }
+        }
+    ) .then(workData => res.json(workData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+    });
+
+router.post('/', (req, res) => {
+    // create a new workout
+    Workout.create(
+        {
+        workout_type: req.body.workout_type
+        }
+    ) .then(workData => res.json(workData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    });
+    
+    router.put('/:id', (req, res) => {
+    // update a workout by its `id` value
+    Workout.update(req.body, {
+        where: {
+        id: req.params.id
+        }
+    })
+    .then(workData => {
+        if (!workData) {
+        res.status(404).json({ message: 'No workout found with this id' });
+        return;
+        }
+        res.json(workData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+    router.delete('/:id', (req, res) => {
+    // delete a workout by its `id` value
+    Workout.destroy(
+        {
+        where: {
+            id: req.params.id
+        }
+        }
+    ) .then(workData => {
+        if(!workData) {
+        res.status(404).json({ message: 'No workout found with this id' });
+        return;
+        }
+        res.json(workData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+    module.exports = router;
