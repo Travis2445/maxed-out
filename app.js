d5f32f53b06eab289c64fccad99502485a282452
Jessica Hille <jessicalhille@gmail.com>
2022-01-27 20:23:52 -0600

    fixed login, merged to develop

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
