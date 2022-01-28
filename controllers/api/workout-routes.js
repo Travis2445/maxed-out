7f283d3cbc6f3b8eee33ee9d07507af09a97f617
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2022-01-27 19:41:06 -0600

    finished cardio-routes and cleaned up some formatting in workout-routes

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -1,79 +1,30 @@
 const router = require('express').Router();
-
 const { Workout, Weights, Cardio } = require('../../models');
+const sequelize = require('../../config/connection');
 
+// GET /api/workout
 router.get('/', (req, res) => {
-        // find all wokrouts
-        Workout.findAll()
-        .then(workData => res.json(workData))
-        .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-        });
-    });
-
-    router.get('/:id', (req, res) => {
-    // find one workout by its `id` value
-    Workout.findOne(
-        {
-            where: {
-                id: req.params.id
-            },
-        }
-    ) .then(workData => res.json(workData))
-        .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-        });
-    });
-
-router.post('/', (req, res) => {
-    // create a new workout
-    Workout.create(
-        {
-        workout_type: req.body.workout_type
-        }
-    ) .then(workData => res.json(workData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-    });
-    
-    router.put('/:id', (req, res) => {
-    // update a workout by its `id` value
-    Workout.update(req.body, {
-        where: {
-        id: req.params.id
-        }
-    })
-    .then(workData => {
-        if (!workData) {
-        res.status(404).json({ message: 'No workout found with this id' });
-        return;
-        }
-        res.json(workData);
-    })
+    Workout.findAll()
+    .then(dbWorkoutData => res.json(dbWorkoutData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
-    // delete a workout by its `id` value
-    Workout.destroy(
-        {
+// GET /api/workout/:id
+router.get('/:id', (req, res) => {
+    Workout.findOne({
         where: {
             id: req.params.id
         }
+    })
+    .then(dbWorkoutData => {
+        if (!dbWorkoutData) {
+            res.status(404).json({ message: 'No workout found with this id' });
+            return;
         }
-    ) .then(workData => {
-        if(!workData) {
-        res.status(404).json({ message: 'No workout found with this id' });
-        return;
-        }
-        res.json(workData);
+        res.json(dbWorkoutData);
     })
     .catch(err => {
         console.log(err);
@@ -81,4 +32,56 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-    module.exports = router;
+// POST /api/workout
+router.post('/', (req, res) => {
+    Workout.create({
+        workout_type: req.body.workout_type
+    })
+    .then(dbWorkoutData => res.json(dbWorkoutData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT /api/workout/:id
+router.put('/:id', (req, res) => {
+    Workout.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbWorkoutData => {
+        if (!dbWorkoutData) {
+            res.status(404).json({ message: 'No workout found with this id' });
+            return;
+        }
+        res.json(dbWorkoutData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE /api/workout/:id
+router.delete('/:id', (req, res) => {
+    Workout.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbWorkoutData => {
+        if(!dbWorkoutData) {
+            res.status(404).json({ message: 'No workout found with this id' });
+            return;
+        }
+        res.json(dbWorkoutData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
