0219c72c9bf047367f67e2db9c7e37f6c7ebdf89
Jessica Hille <jessicalhille@gmail.com>
2022-01-27 20:16:54 -0600

    login updated and functional

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: 'Super secret secret',
@@ -21,17 +17,21 @@ const sess = {
       db: sequelize
     })
 };
-  
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening on port ', PORT));
 });
