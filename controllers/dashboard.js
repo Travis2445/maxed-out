7042d0e4ae9c8eaf1a5159f057cc81a78ea09778
Jessica Hille <jessicalhille@gmail.com>
2022-01-31 19:38:40 -0600

    css, started dashboard

---
+++ controllers/dashboard.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+const { User, Workout, Weights, Cardio } = require('../models');
+
+router.get('/', withAuth, (req, res) => {
+    Workout.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'workout_type',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Cardio,
+                attributes: ['id', 'cardio_type', 'intensity', 'duration', 'distance', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['email']
+                }
+            },
+            {
+                model: User,
+                attributes: ['email']
+            }
+        ]
+    })
+        .then(dbWorkoutData => {
+            const posts = dbWorkoutData.map(workout => workout.get({ plain: true }));
+            res.render('dashboard', { workout, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = route;
